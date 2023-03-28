585e46f02daa4d9c6b00b3daa2b7d72388bbf961
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-03-28 18:26:38 +0100

    fetch rewriten - app will fetch a list of movies now

--- index.js
+++ index.js
@@ -11,6 +11,8 @@
 // If you did not make this request, please disregard this email.
 
 const form = document.getElementById("form")
+const result = document.getElementById("results")
+const movieTitle  = document.getElementById("movie-title")
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
@@ -19,16 +21,41 @@ form.addEventListener("submit", (e)=>{
 
 
 const fetchMovies = async () => {
-    const response = await fetch('http://www.omdbapi.com/?t=Terminator+2&apikey=182bd55b');
-    const movies = await response.json();
-    console.log(movies)
-    const searchResult = {
-        Poster : movies.Poster,
-        Title : movies.Title,
-        Rating : movies.Ratings[0].Value
-    };
-    console.log(searchResult)
-    
-  }
+    const response = await fetch(`http://www.omdbapi.com/?s=${movieTitle.value}&apikey=182bd55b`);
+    const movie = await response.json();
+    const searchResults = movie.Search
+    searchResults.length = 7
+
+    const resultsWithDetails = []
+    for ( let searchResult of searchResults){
+        const response = await fetch(`http://www.omdbapi.com/?t=${searchResult.Title}&apikey=182bd55b`);
+        const movies = await response.json();
+        resultsWithDetails.push(movies)
+    }
+    console.log(resultsWithDetails)
 
 
+    let html = ''
+    for (let movie of resultsWithDetails) {
+        html += `<div class="w-[454px] flex border-b border-b-[#2E2E2F] py-6 mb-3">
+        <img
+          src="${movie.Poster}"
+          alt="movie poster"
+          class="w-[100px] h-[150px] inline-block mr-6"
+        />
+        <div class="inline-block w-[300px] text-white">
+          <h1 class="inline text-xl mr-3 font-semibold mt-9">${movie.Title}</h1>
+          <p class=" inline mr-5 text-lg">${movie.Ratings[0].Value}⭐️</p>
+          <br>
+          <p class="inline text-xs mr-3">${movie.Runtime}</p>
+          <p class="inline text-xs mr-3">${movie.Genre}</p>
+          <button class="text-xs mt-3 font-bold text-[#434344] hover:text-white">➕ Watchlist</button>
+          <p class="text-xs mt-3 text-[#434344] ">${movie.Plot}</p>
+        </div>
+      </div>`
+    }
+    result.innerHTML = html
+}
+
+
+   
